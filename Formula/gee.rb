4cbdaae1d12355e3df7ce04edb0b47170c486101
Ammon Taylor <48261561+human37@users.noreply.github.com>
2021-04-15 15:11:18 -0600

    Update gee.rb

--- Formula/gee.rb
+++ Formula/gee.rb
@@ -4,9 +4,9 @@
 class Gee < Formula
   desc "CLI repository manager and automation tool written in rust."
   homepage "https://github.com/human37/gee"
-  url "https://github.com/human37/gee/releases/download/v1.1/gee-mac.tar.gz"
-  sha256 "a04941f334c31d396f9baad9cfcacdfd8dbb80672e589cf6a37b964a5620c240"
-  version "1.1"
+  url "https://github.com/human37/gee/releases/download/v1.2/gee-mac.tar.gz"
+  sha256 "9c0f276b057ba37943edafe5d5ad86a3a470e3c266f47b1855b0b2f95596b3e9"
+  version "1.2"
 
   def install
     bin.install "gee"
